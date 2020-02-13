1fc305c2c9163ca1f9767459c35aa63fe2cfc492
Jocelyn Afandi <jafandi@Jocelyns-MacBook-Pro.local>
2020-02-12 19:51:11 -0800

    fixed all feedback points we hope

---
+++ js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function openHamburger() {
+    var nav = document.getElementById("nav-items");
+    if (nav.style.display == "block") {
+        nav.style.display == "none"; 
+    } else {
+        nav.style.display == "block";
+    }
+}
