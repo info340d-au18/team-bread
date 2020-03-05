155aae436c760754573032e26d1decaf5a9c83de
Jocelyn Afandi <jafandi@Jocelyns-MacBook-Pro.local>
2020-03-04 20:41:27 -0800

    Updates

---
+++ precache-manifest.2dffbfb924dd00ddbe39ce9f4e60cbb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5158696f6b0c8e10562609905b815189",
+    "url": "/team-bread/index.html"
+  },
+  {
+    "revision": "8572a876c690760a475a",
+    "url": "/team-bread/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "7851833e32d1e75de609",
+    "url": "/team-bread/static/css/main.ae8b2bb9.chunk.css"
+  },
+  {
+    "revision": "8572a876c690760a475a",
+    "url": "/team-bread/static/js/2.b74bc882.chunk.js"
+  },
+  {
+    "revision": "acb29fef48adc9c5bcfac36459560713",
+    "url": "/team-bread/static/js/2.b74bc882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7851833e32d1e75de609",
+    "url": "/team-bread/static/js/main.7286d85e.chunk.js"
+  },
+  {
+    "revision": "9fc4088a66c0ffdd76e3",
+    "url": "/team-bread/static/js/runtime-main.d3b39b16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/team-bread/static/media/logo.5d5d9eef.svg"
+  }
+]);
